bb13cbdc156f9f4a110423cb531057140ca1e200
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-03-26 01:40:53 +0800

    Updated Tutorial 1 2.py

--- RE1016/Tutorial 1/2.py
+++ RE1016/Tutorial 1/2.py
@@ -1,3 +1,9 @@
+# Consider a triangle with sides of length 3, 7, and 9. The law of cosines states that given three sides 
+# of a triangle (a, b, and c) and the angle C between sides a and b: 
+# 𝑐**2 = 𝑎**2 + 𝑏**2 − 2 ∗ 𝑎 ∗ 𝑏 ∗ cos (𝐶)
+# Write Python code to calculate the three angles in the triangle.
+# Hint: math module will be useful
+
 import math
 
 a = 3
