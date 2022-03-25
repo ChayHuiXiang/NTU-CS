0fe744ce5bc22415b92ad624b5438c4357a3e060
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-03-26 01:39:44 +0800

    Updated Tutorial 1 1.py

--- RE1016/Tutorial 1/1.py
+++ RE1016/Tutorial 1/1.py
@@ -1,3 +1,7 @@
+# A day has 86,400 secs (24*60*60). Given a number in the range 1 – 86,400, output the current time 
+# as hours, minutes, and seconds with a 24-hour clock. For example, 70,000 sec is 19 hours, 26 minutes, 
+# and 40 seconds.
+
 number = input('Number: ')
 number = int(number)
 hours = number/3600
