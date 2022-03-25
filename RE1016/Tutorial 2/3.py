491cb7079476b2c50dc3f4a59b878b165d12989c
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-03-26 01:44:08 +0800

    Updated Tutorial 2 3.py

--- RE1016/Tutorial 2/3.py
+++ RE1016/Tutorial 2/3.py
@@ -1,3 +1,7 @@
+# Write a program that continually generates and sums up random numbers between 1 and 20 and 
+# stops when the sum exceeds 1000. Stop early if more than 100 numbers have been generated.
+# Hint: random module will be useful
+
 import random
 sum = count = 0
 while sum <= 1000 and count < 100:
