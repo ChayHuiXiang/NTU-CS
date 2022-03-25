0d23c6d1cae083144581909e2355ceac5e4486d4
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-03-26 01:43:21 +0800

    Updated Tutorial 3 1.py

--- RE1016/Tutorial 3/1.py
+++ RE1016/Tutorial 3/1.py
@@ -1,3 +1,10 @@
+# Jacob has been passing notes in class without a problem until his teacher intercepted one today. In 
+# order to prevent future embarrassment, he wants a way to encode his notes so his teacher cannot 
+# read them. Help him by writing a program that encrypts a string by switching every letter with 
+# alphabetical position i with the letter with alphabetical position 26-i. It should be letter ‘m’ = 12
+# becomes the letter ‘o’ = 14.
+# Hint: string module will be useful.
+
 import string
 
 stringy = input('Please key in a string: ')
