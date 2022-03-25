64015a2714c23cf624d1966716a5c109a1cf0411
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-03-26 01:41:29 +0800

    Updated Tutorial 2 1.py

--- RE1016/Tutorial 2/1.py
+++ RE1016/Tutorial 2/1.py
@@ -1,3 +1,7 @@
+# Write a program that prints all the leap years from 1900 to 2020 (inclusive).
+# Hint: A leap year in the Gregorian calendar system is a year that is divisible by 4 but not by 100, 
+# unless it is also divisible by 400.
+
 for i in range(1900,2021,4):
     if i%100 != 0 or i%400 == 0:
         print(i)
