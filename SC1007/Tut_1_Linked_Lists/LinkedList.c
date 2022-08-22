b31c1b014fed2aa964605226fa8d31ea1176d418
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-08-22 22:44:14 +0800

    Edited LinkedList

--- SC1007/Tut_1_Linked_Lists/LinkedList.c
+++ SC1007/Tut_1_Linked_Lists/LinkedList.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
-int main() {
-  struct person {
+struct person {
+  char firstName [15];
+  char lastName [15];
+  struct {
+    int age ;
+    float height ;
+    float weight ;
     char firstName [15];
-    char lastName [15];
-    struct {
-      int age ;
-      float height ;
-      float weight ;
-      char firstName [15];
-    } * InfoPtr, Info ;
-    struct person *personP ;
-  } student1 ;
-  typedef struct person person_t ;
-  person_t * studentPtr = & student1 ;
-  person_t ** studentPtrPtr = & studentPtr ;
+  } * InfoPtr, Info ;
+  struct person *personP ;
+} student1 ;
+typedef struct person person_t ;
+
+int main() {
+
+  person_t* studentPtr = &student1 ;
+  person_t** studentPtrPtr = &studentPtr ;
 
   student1.Info.age = 15;
 
-  printf("%d", studentPtr->Info.age);
+  student1.InfoPtr = &student1.Info;
 
-  printf("%d", ((person_t*)*studentPtrPtr)->Info.age);
+  printf("%d\n", studentPtr->InfoPtr->age);
+
+  printf("%d\n", studentPtr->Info.age);
+
+  printf("%d\n", ((person_t*)*studentPtrPtr)->Info.age);
 
   return 0;
 }
