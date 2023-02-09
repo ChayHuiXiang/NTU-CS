1a071b0e7b728f6a22bc31dda6d76e9c3e01deeb
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-02-09 16:06:26 +0800

    Edit Lab3

---
+++ SC2001/Lab_1/Lab1.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 10000000
+#define SORT_THRESHOLD 100
+
+void insertionSort(int arr[], int low, int high) {
+    int i, j, key;
+    for (i = low + 1; i <= high; i++) {
+        key = arr[i]; // number to insert to the sorted array
+        j = i - 1;
+
+        // check elements from the right of sorted array whether it's larger than key,
+        // shift element to the right by one slot if larger
+        while (j >= low && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        // insert number into the right slot of the sorted array
+        arr[j + 1] = key;
+    }
+}
+
+void merge(int* arr, int low, int mid, int high, int *count) {
+    int i, j, k;
+    int n1 = mid - low + 1; // size of Left Array
+    int n2 = high - mid; // size of Right Array (high - mid + 1 - 1) = high - mid
+
+    int* L = (int*)malloc(sizeof(int) * n1); // initialise left array
+    int* R = (int*)malloc(sizeof(int) * n2); // initialise right array
+
+    // copy over items from left sorted array
+    for (i = 0; i < n1; i++)
+        L[i] = arr[low + i]; 
+
+    // copy over items from right sorted array
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+    
+    i = 0;
+    j = 0;
+    k = low;
+
+    while (i < n1 && j < n2) {
+        (*count)++;
+        // compare first item from left array with first item from right array, copy smaller item over
+        // repeats until either left or right array is empty
+        if (L[i] <= R[j]) { 
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    // copy remaining items from left array
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // copy remaining items from right array
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void hybridSort(int* arr, int low, int high, int *count) {
+    // use insertion sort if array is smaller than SORT_THRESHOLD
+    if (high - low <= SORT_THRESHOLD) {
+        insertionSort(arr, low, high);
+    // use merge sort if array is bigger than SORT_THRESHOLD
+    } else if (low < high) {
+        int mid = (low + high) / 2;
+        hybridSort(arr, low, mid, count);
+        hybridSort(arr, mid + 1, high, count);
+        merge(arr, low, mid, high, count);
+    }
+}
+
+int main() {
+    // int n, i, count = 0;
+    // for (n = 1000; n <= MAX; n *= 10) {
+    //     int* arr = (int*)malloc(sizeof(int) * MAX);
+    //     srand(time(NULL));
+    //     for (i = 0; i < n; i++) {
+    //         arr[i] = rand() % n + 1;
+    //     }
+    //     count = 0;
+    //     hybridSort(arr, 0, n - 1, &count);
+    //     printf("For n = %d, number of key comparisons = %d\n", n, count);
+    // }
+    int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    insertionSort(arr, 0, 9);
+    return 0;
+}
---
+++ SC2001/Lab_1/Lab1.py
@@ -0,0 +1,57 @@
+import random
+SORT_THRESHOLD = 100
+
+def merge(left, right, count):
+  sorted_arr = []
+  left_pointer = 0
+  right_pointer = 0
+  left_len = len(left)
+  right_len = len(right)
+
+  while left_pointer < left_len and right_pointer < right_len:
+    left_item = left[left_pointer]
+    right_item = right[right_pointer]
+    count += 1
+    if left_item <= right_item:
+      sorted_arr.append(left_item)
+      left_pointer += 1
+    else:
+      sorted_arr.append(right_item)
+      right_pointer += 1
+
+  sorted_arr.extend(left[left_pointer:left_len])
+  sorted_arr.extend(right[right_pointer:right_len])
+  return sorted_arr, count
+
+def hybridsort(arr):
+  length = len(arr)
+  if length <= SORT_THRESHOLD:
+    return insertionsort(arr)
+
+  mid = length // 2
+  left, leftCount = hybridsort(arr[:mid])
+  right, rightCount = hybridsort(arr[mid:])
+  
+  return merge(left, right, leftCount + rightCount)
+
+def insertionsort(arr):
+  count = 0
+  length = len(arr)
+  for i in range(1, length):
+    for j in range(i, 0, -1):
+      count += 1
+      if arr[j] < arr[j-1]:
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+      else:
+        break
+  return arr, count
+
+if __name__ == "__main__":
+  START = 1000
+  END = 10000000
+  while START <= END:
+    arr = []
+    for i in range(START):
+      arr.append(random.randint(0, START))
+    answer, count = insertionsort(arr)
+    START *= 10
